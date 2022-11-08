61196fefab7feeef703d5b4ff790b9068d95beed
Minha Kwon <namisoft@naver.com>
2022-11-09 00:20:38 +0900

    navbar 수정

--- layouts/Navbar.js
+++ layouts/Navbar.js
@@ -3,27 +3,27 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div>
-      <div className="navbar grid grid-cols-3 tracking-wide my-2">
+      <div className="navbar grid grid-cols-2 tracking-wide my-2">
         <div className="logo flex justify-start">
           <h3 className="font-extrabold text-3xl">
             <Link href="/">ChartSimulator</Link>
           </h3>
         </div>
-        <div className="navs flex justify-center font-bold  p-2 hidden lg:flex">
+        {/* <div className="navs flex justify-center font-bold  p-2 hidden lg:flex">
           <div className="mx-2">
             <Link href="/">Home</Link>
           </div>
           <div className="mx-2">
             <Link href="/simulator">Simulator</Link>
-          </div>
+          </div> */}
 
-          {/* <div className="mx-2">
+        {/* <div className="mx-2">
             <Link href="/guide">Guide</Link>
           </div> */}
-          {/* <div className="mx-2">
+        {/* <div className="mx-2">
             <Link href="https://discord.gg/Ga3kp745r5">Discord</Link>
           </div> */}
-        </div>
+        {/* </div> */}
         <div className="flex justify-end font-bold px-2 flex content-end col-start-3  ">
           <div className="login-button py-2 mx-2 hidden md:flex cursor-pointer">
             <Link href="/api/auth/login">Log in</Link>
