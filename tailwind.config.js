e180d4fa503ddfef9e23810f17e00c4c21aab37b
Minha Kwon <namisoft@naver.com>
2022-11-14 15:12:32 +0900

    tailwind content 경로 설정 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
   // mode: "jit",
   // purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
