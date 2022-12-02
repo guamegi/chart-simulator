a79450de9ffbad9c8acf6384ddd2fb9275965b4f
Minha Kwon <namisoft@naver.com>
2022-12-02 18:04:42 +0900

    footer 사이즈, 색상 수정

--- components/layouts/footer.js
+++ components/layouts/footer.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+// import Image from "next/image";
 // import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center w-full h-24 border-t">
+    <footer className="flex items-center justify-center text-sm text-gray-500 w-full h-24 border-t">
       2022 © Copyright ChartSimulator. All rights reserved.
     </footer>
   );
